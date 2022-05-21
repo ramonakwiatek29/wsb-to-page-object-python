a34d800c853a64feb06e40800c91d792c08bb87a
Dawid Okuniewski <mgr.dawid.okuniewski@gmail.com>
2022-05-21 21:10:44 +0200

    #2 basepage created

---
+++ pages/base_page.py
@@ -0,0 +1,39 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def go_to(self, url):
+        self.driver.get(url)
+
+    def get_current_url(self):
+        return self.driver.current_url
+
+    def find_element(self, locator, wait=0):
+        element = WebDriverWait(self.driver, wait).until(ec.presence_of_element_located((locator['by'], locator['value'])))
+
+    def find_elements(self, locator):
+        elements = self.driver.find_elements(by=locator['by'], value=locator['value'])
+        return elements
+
+    def fill(self, locator, value='', wait=0):
+        element = WebDriverWait(self.driver, wait).until(ec.presence_of_element_located((locator['by'], locator['value'])))
+        element.send_keys(value)
+
+    def click_btn(self, locator, wait=0):
+        btn = WebDriverWait(self.driver, wait).until(ec.element_to_be_clickable((locator['by'], locator['value'])))
+        btn.click()
+
+    def click_checkbox(self, locator, selected=False):
+        if selected:
+            btn = self.driver.find_element(locator['by'], locator['value'])
+            self.driver.execute_script("arguments[0].click();", btn)
+
+    def get_message(self, locator, wait=0):
+        element = WebDriverWait(self.driver, wait).until(ec.presence_of_element_located((locator['by'], locator['value'])))
+        msg = element.text
+        return msg
