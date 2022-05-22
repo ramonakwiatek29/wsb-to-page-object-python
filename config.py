5c265e88dc08febcb641a7f69de0f70da1cafe8f
Daria Grabska <daria_grabska@wp.pl>
2022-05-22 13:00:59 +0200

    locators for item and cart pages

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 # URL
 REGISTRATION_URL = 'https://www.sklepdemo.pl/rejestracja'
+CART_PAGE_URL = 'https://www.sklepdemo.pl/cart'
+ITEM_PAGE = 'https://www.sklepdemo.pl/cudny-sweterek-z-nanotechnologicznych-wlokien-id-77'
+
