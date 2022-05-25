b094fcc75fc9bb10102caab41ebba4fa71cb3b88
Weronika Press <weronikapress@gmail.com>
2022-05-25 19:14:25 +0000

    login-locators-added

--- pages/locators.py
+++ pages/locators.py
@@ -181,3 +181,37 @@ BOOKS_LOCATORS = {
     }
 
 }
+
+LOGIN_LOCATORS = {
+    'login_form': {
+        'email': {
+            'by': By.ID,
+            'value': 'login',
+            'error_msg': {
+                'by': By.CSS_SELECTOR,
+                'value': 'label[for="login"] + div.form-hint'
+            }
+        },
+        'password':{
+            'by': By.ID,
+            'value': 'password',
+            'error_msg': {
+                'by': By.CSS_SELECTOR,
+                'value': 'label[for="password"] + div.form-hint'
+            }
+        },
+        'submit_btn':{
+            'by': By.XPATH,
+            'value': '//button[@type="submit" and contains(text(), "Zaloguj się")]'
+        }
+    },
+
+    'validation_error_msgs': {
+        'by': By.CSS_SELECTOR,
+        'value': 'form  div.form-hint'
+    },
+    'login_error_msg': {
+        'by': By.CLASS_NAME,
+        'value': 'infobox-message'
+    },
+}
