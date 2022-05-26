b0228d5309a94c324fed7abd2adc30dce3bf07b4
Weronika Press <weronikapress@gmail.com>
2022-05-26 20:58:55 +0200

    login-messages-added

--- tests/messages.py
+++ tests/messages.py
@@ -9,3 +9,12 @@ REGISTRATION_MESSAGES = {
             'success_msg': 'Konto zostało poprawnie aktywowane, zapraszamy do zakupów!'
         }
     }
+LOGIN_MESSAGES = {
+        'error_msgs': {
+            'email': 'Podaj prawidłowy email',
+            'password': 'Co najmniej 5 znaków (bez spacji)',
+        },
+        'success_msgs': {
+            'success_msg': 'Konto zostało poprawnie aktywowane, zapraszamy do zakupów!'
+        }
+    }
